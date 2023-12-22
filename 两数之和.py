70a2479c24d2a5a9c89b0d833268d4d28335a50d
DDing666 <54842941+DDing666@users.noreply.github.com>
2023-12-22 19:00:09 +0800

    12.22

--- 两数之和.py
+++ 两数之和.py
@@ -1,3 +1,9 @@
+#https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+'''
+这里的是将value作为index来减少复杂度
+即创建num的这样的一个哈希表
+'''
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num = {}
@@ -6,4 +12,15 @@ class Solution:
                 num[value] = index
             else:
                 return [index, num[target - value]]
+        return []   
+'''
+题解
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
         return []
+'''
